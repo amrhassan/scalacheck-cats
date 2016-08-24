5cc8291fbbbc608178179e1aa25f5bddd62241df
Amr Hassan <amr.hassan@gmail.com>
2016-08-24 19:21:07 +0200

    Cats 0.7.0 compatibility

--- src/main/scala/scalacheck/cats/Instances.scala
+++ src/main/scala/scalacheck/cats/Instances.scala
@@ -7,6 +7,7 @@ trait Instances {
   implicit val genMonad: Monad[Gen] = new Monad[Gen] {
     def pure[A](a: A): Gen[A] = Gen.const(a)
     def flatMap[A, B](fa: Gen[A])(f: A => Gen[B]): Gen[B] = fa flatMap f
+    def tailRecM[A, B](a: A)(f: (A) ⇒ Gen[Either[A, B]]): Gen[B] = defaultTailRecM(a)(f)
   }
 }
 
